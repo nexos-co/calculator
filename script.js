3fde81378500a9c694ef1eb6ff1d56e891de7559
leyddy <leyddy>
2025-01-28 16:37:41 -0500

    ready

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 const inputValue = document.getElementById("user-input");
-const number = document.querySelectorAll(".numbers").forEach(function (item) {
-    item.addEventListener("click", function (e) {
-        if (inputValue.innerText === "NaN") {
-            inputValue.innerText = "";
-        }
-        if (inputValue.innerText === "0") {
-            inputValue.innerText = "";
-        }
-        inputValue.innerText += e.target.innerHTML.trim();
-    });
+const outputValue = document.querySelector('.display .output');
+
+const number = document.querySelectorAll(".key").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    if (inputValue.innerText === "NaN") {
+      inputValue.innerText = "";
+      outputValue.innerText = "";
+    }
+    if (inputValue.innerText === "0") {
+      inputValue.innerText = "";
+      outputValue.innerText = "";
+    }
+    inputValue.innerText += e.target.innerHTML.trim();
+  });
 });
 
 const calculate = document
-    .querySelectorAll(".operations")
-    .forEach(function (item) {
-        item.addEventListener("click", function (e) {
-            let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+  .querySelectorAll(".op")
+  .forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+      if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+        if (inputValue.innerText.includes("^")) {
+          inputValue.innerText = inputValue.innerText.replace("^", "**");
+        }
+        outputValue.innerText = eval(inputValue.innerText);
+      }
+      else if (e.target.innerHTML === "AC") {
+        inputValue.innerText = "";
+        outputValue.innerText = "";
+      } else if (e.target.innerHTML === "DEL") {
+        inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
+        if (inputValue.innerText.length == 0) {
+          inputValue.innerText = "";
+        }
 
-            if (!isNaN(lastValue) && e.target.innerHTML === "=") {
-                inputValue.innerText = eval(inputValue.innerText);
-            } else if (e.target.innerHTML === "AC") {
-                inputValue.innerText = 0;
-            } else if (e.target.innerHTML === "DEL") {
-                inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
-                if (inputValue.innerText.length == 0) {
-                    inputValue.innerText = 0;
-                }
-            } else {
-                if (!isNaN(lastValue)) {
-                    inputValue.innerText += e.target.innerHTML;
-                }
-            }
-        });
+      } else {
+        if (!isNaN(lastValue)) {
+          inputValue.innerText += e.target.innerHTML;
+        }
+      }
+
     });
+  });
+
